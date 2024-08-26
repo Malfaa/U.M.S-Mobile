0e0e8ddf381e02642f2d413a2bbd1b635f852624
Malfaa <gmalfacorrea@gmail.com>
2024-08-25 23:31:28 -0300

    feat: Viewmodel created

---
+++ app/src/main/java/com/ums/ums_mobile/viewmodel/UserViewmodel.kt
@@ -0,0 +1,10 @@
+package com.ums.ums_mobile.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class UserViewmodel: ViewModel(){
+
+//    private val _count = MutableStateFlow(0)
+//    val count: StateFlow<Int> get() = _count
+
+}
